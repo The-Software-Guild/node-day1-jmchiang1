fc2a014ec6b127ba09f2c224977fde9104dfa68b
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-12-08 09:47:26 -0500

    separate object into displaying id, createdat, and name

--- Express.js
+++ Express.js
@@ -1,20 +1,59 @@
 //documentation: https://dev.to/miku86/nodejs-how-to-create-a-simple-server-using-express-1n9d
+//documentation: https://codehandbook.org/how-to-make-rest-api-calls-in-express-web-app/
 
 const express = require('express');
-const api = require('./API')
-const https = require('https');
+const request = require("request");
 
 const app = express();
 const PORT = 3000;
 
-// app.use(bodyParser.urlendcoded({ extended: false }));
-// app.use(bodyParser.json());
+function apiHelper (url) {
+    return new Promise((resolve, reject) => {
+      request(url, { json: true }, (err, res, body) => {
+        if (err) reject(err);
+        resolve(body);
+      });
+    });
+  };
 
+//Display entire JSON Object
 app.get('/', (req, res) => {
-    api.make_API_call('http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees')
+    apiHelper('http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees')
     .then(response => {
         res.json(response);
-        // res.json(response.map(data => data.name))
+    })
+    .catch(error => {
+        res.send(error)
+    })
+})
+
+//Display Users
+app.get('/users', (req, res) => {
+    apiHelper('http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees')
+    .then(response => {
+        res.json(response.map(data => data.name))
+    })
+    .catch(error => {
+        res.send(error)
+    })
+})
+
+//Display IDs
+app.get('/id', (req, res) => {
+    apiHelper('http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees')
+    .then(response => {
+        res.json(response.map(data => data.id))
+    })
+    .catch(error => {
+        res.send(error)
+    })
+})
+
+//Display CreatedAt
+app.get('/created', (req, res) => {
+    apiHelper('http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees')
+    .then(response => {
+        res.json(response.map(data => data.createdAt))
     })
     .catch(error => {
         res.send(error)
--- README.md
+++ README.md
@@ -21,6 +21,6 @@
 [x] Application will make a call to the following API endpoint (http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees) using Request Object.
 [x] Once the data is available, the employee list along with the details should be visible to the User on the client side front end wed page.
 [x] The employee details would contain:
-    - Name
-    - Id
-    - CreatedAt
+    [x] Name
+    [x] Id
+    [x] CreatedAt
